9c00a3b12a609b274ae2ac4fef6e19878cfb4562
wahomeJohn <johnweru206@gmail.com>
2025-07-23 11:02:37 +0300

    Client changes..add shadcn ui

--- src/features/landlord/components/caretakers/AddCaretakerModal.tsx
+++ src/features/landlord/components/caretakers/AddCaretakerModal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react";
 import { X } from "lucide-react";
